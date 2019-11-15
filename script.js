3379a6f41ec941cec16ecdf1e9d89e2b984fcd5f
John Pham <mr.john.pham@gmail.com>
2019-11-15 11:33:23 -0500

    Create and render MyComponent

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+let MyComponent = function MyComponent(props) {
+    return React.createElement(
+        "div",
+        null,
+        "Hello",
+        props.name
+    )
+};
 
+ReactDOM.render(React.createElement(MyComponent, {name: "John"}), app);

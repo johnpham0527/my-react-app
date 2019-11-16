5c93423994974d8b4fd16f87fb4fc205446d8266
John Pham <mr.john.pham@gmail.com>
2019-11-16 14:30:30 -0500

    Implement props

--- script.js
+++ script.js
@@ -5,11 +5,29 @@ class MyComponent extends React.Component {
         super(props);
     }
     render() {
-        //return React.createElement("div", null, "Hello", props.name)
-        return React.createElement("div", null, "Hello");
+        return React.createElement("div", null, "Hello ", this.props.name)
+        //return React.createElement("div", null, "Hello");
     }
 }
 
-//const domContainer = document.querySelector('#app');
-//ReactDOM.render(React.createElement(MyComponent), domContainer);
-ReactDOM.render(React.createElement(MyComponent), document.querySelector("#app"));
+class Hello extends React.Component {
+    render() {
+        return React.createElement(
+            'div',
+            null,
+            `Hello ${this.props.toWhat}`
+        );
+    }
+}
+
+/*
+ReactDOM.render(
+    React.createElement(Hello, { toWhat: 'World' }, null),
+    document.querySelector("#app")
+);
+*/
+
+ReactDOM.render(
+    React.createElement(MyComponent, { name: 'John' }, null), 
+    document.querySelector("#app")
+);

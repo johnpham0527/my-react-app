01fed17ccd0db944b4e4a6375cb5a5c6e6d8a279
John Pham <mr.john.pham@gmail.com>
2019-12-15 08:34:56 -0500

    Add onClick to calc equal button

--- script.js
+++ script.js
@@ -734,7 +734,8 @@ class MyComponent extends React.Component {
                                 backgroundColor: "#eeeeee",
                                 padding: "10px",
                                 textAlign: "center"
-                            }
+                            },
+                            onClick: () => this.handleCalcButton("equal")
                         },
                         "="
                     )

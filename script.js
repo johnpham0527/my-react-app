7653a0fe2431ac4323413b22109bc3194d9be736
John Pham <mr.john.pham@gmail.com>
2020-01-16 14:58:02 -0500

    Update to-do list: calcReducer decimal error check

--- script.js
+++ script.js
@@ -996,5 +996,5 @@ JavaScript calculator to-do list:
     [X] Re-implement the divide button
     [ ] Re-implement the equal button code to test what I currently have
     [ ] Insert code to "overwrite" operands and not allow two consecutive operands to be pushed into the queue
-    [ ] Perform error checks for calcReducer decimal case 
+    [X] Perform error checks for calcReducer decimal case 
 */

8b127c1d852440376b889cc41b10e7082bec9448
John Pham <mr.john.pham@gmail.com>
2020-03-05 04:15:46 -0500

    Create pomodor timer to-do list

---
+++ timerScript.js
@@ -0,0 +1,64 @@
+
+
+/*
+Pomodoro Timer To-do's
+[ ] Implement Redux code
+    [ ] Implement constant variables
+        [ ] Implement MAX break session value
+        [ ] Implement MIN break session value
+        [ ] Implement MAX alarm timer value
+        [ ] Implement MIN alarm timer value
+        [ ] Implement reducer function constants
+            [ ] Break session increment
+            [ ] Break session decrement
+            [ ] Alarm timer set value increment
+            [ ] Alarm timer set value decrement
+            [ ] Alarm activated
+            [ ] Active alarm paused
+            [ ] Reset
+    [ ] Implement default state object with the following properties
+        [ ] Break session value ({minutes, seconds} object?)
+        [ ] Alarm timer set value ({minutes, seconds} object?)
+        [ ] Is alarm active?
+        [ ] (Should the audio file be triggered?)
+    [ ] Implement action creators
+        [ ] Break session value increment
+        [ ] Break session value decrement
+        [ ] Alarm timer set value increment
+        [ ] Alarm timer set value decrement
+        [ ] Alarm has been activated
+        [ ] Active alarm has been paused
+        [ ] Reset button has been pushed
+    [ ] Implement pomodoro timer reducer
+        [ ] Break session value increment
+        [ ] Break session value decrement
+        [ ] Alarm timer set value increment
+        [ ] Alarm timer set value decrement
+        [ ] Alarm has been activated
+        [ ] Active alarm has been paused
+        [ ] Reset button has been pushed
+        [ ] Default state
+    [ ] Implement helper functions
+        [ ] (Convert seconds to {minutes, seconds} object?)
+        [ ] (Convert {minutes, seconds} object to seconds?)
+[ ] Implement React-Redux code
+    [ ] Implement app wrapper
+    [ ] Map Redux state to React props
+    [ ] Map Redux dispatchers to React props
+[ ] Implement React code template
+    [ ] Implement constructor
+    [ ] Implement break session increment and decrement buttons
+    [ ] Implement break session display
+    [ ] Implement set timer increment and decrement buttons
+    [ ] Implement set timer display
+    [ ] Implement active alarm timer
+[ ] Learn how to implement a timer in vanilla JavaScript
+    [ ] Learn about set interval
+[ ] Learn how to play an HTML5 audio file in React
+    [ ] How did I implement this with the drum project?
+[ ] Implement CSS
+    [ ] Create pomodoro.css
+    [ ] Implement button styles
+    [ ] Implement break session, set timer, and active alarm timer display styles
+
+*/

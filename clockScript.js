b60a603a83ec7a1d87485283fcd8d92e5059e7b1
John Pham <mr.john.pham@gmail.com>
2020-03-22 04:55:53 -0400

    Complete to-do: let the React code handle the clock logic

--- clockScript.js
+++ clockScript.js
@@ -718,8 +718,8 @@ ReactDOM.render(
     [X] Implement set timer display
     [X] Implement active alarm timer
     [ ] Clean up HTML: break up the huge block of createElements into smaller HTML const variables
-    [ ] Let the React code handle whether the clock is active or not in local state
-        [X] React Redux timer example: https://codesandbox.io/s/vigilant-wildflower-3yvux
+    [X] Let the React code handle whether the clock is active or not
+    [X] React Redux timer example: https://codesandbox.io/s/vigilant-wildflower-3yvux
     [X] Learn about set interval
     [X] Implement a simple countdown timer using set interval
     [X] Implement reset timer button

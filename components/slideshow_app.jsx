9d804cfeefd3088b67d5adf7a7ecd1f5b2644de9
John Pham <mr.john.pham@gmail.com>
2020-04-14 06:16:06 -0400

    Create slideshow_app.jsx

---
+++ components/slideshow_app.jsx
@@ -0,0 +1,9 @@
+var React = require('react');
+
+class App extends React.Component {
+    render() {
+        return <h1>Testing, testing. 1 2 3.</h1>;
+    }
+}
+
+module.exports = App;
--- slideshow.jsx
+++ slideshow.jsx
@@ -1,5 +1,10 @@
 'use strict';
 
-let React = require('react');
-let ReactDOM = require('react-dom');
+var React = require('react');
+var ReactDOM = require('react-dom');
+var App = require('./components/App');
 
+ReactDOM.render(
+    <App />
+    document.getElementById('app');
+)

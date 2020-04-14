03fd1833339e56ff80c5bc31a97b84187c145536
John Pham <mr.john.pham@gmail.com>
2020-04-14 07:01:18 -0400

    Fix issue: ./app/index.js

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ var HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
 });
 
 module.exports = {
-    entry: __dirname + 'app/index.js',
+    entry: __dirname + './app/index.js',
     module: {
         rules: [
             {

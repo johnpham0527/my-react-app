f788d5047b98d76bb0a6bb21fcb1ca87900e1033
John Pham <mr.john.pham@gmail.com>
2020-04-15 05:49:21 -0400

    Create featurePictures array and URL variable

--- app/components/App.js
+++ app/components/App.js
@@ -1,6 +1,17 @@
 var React = require('react');
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+        const featurePictures = [
+            "feature_back_of_head.jpg"
+        ];
+        const URL = "http://www.john-pham.com/audreybergensten/feature/"
+    }
+    
+    
+    
+
     render() {
         return <h1>Testing, testing. 1 2 3. Hello world!</h1>;
     }

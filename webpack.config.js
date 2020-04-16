48a5bac9423f25e7e7966a50f5361e44122accbb
John Pham <mr.john.pham@gmail.com>
2020-04-16 06:40:12 -0400

    Update webpack to load images

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,14 @@ module.exports = {
                     /other_projects/
                 ],
                 loader: 'babel-loader'
+            },
+            {
+                test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+                exclude: [
+                    /node_modules/,
+                    /other_projects/
+                ],
+                loader: 'url-loader?limit=100000'
             }
         ]
     },
